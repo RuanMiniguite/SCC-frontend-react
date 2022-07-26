52c5e4d810a013ae53dc245cd6f56dbe8e6b4c4d
Patricia Darós <prdaros@gmail.com>
2022-07-26 14:28:50 -0300

    :package: Organização de Arquivos

--- scc-frontend-react/src/pages/CRUD/Cliente.jsx
+++ scc-frontend-react/src/pages/CRUD/Cliente.jsx
@@ -1,5 +1,5 @@
-import { Sidebar } from "../components/Sidebar";
-import { Header } from "../components/Header";
+import { Sidebar } from "../../components/Sidebar";
+import { Header } from "../../components/Header";
 
 export function Cliente() {
   return (

6a52a9bfc5dfee9ecc63daf056de91282c1f03ed
RuanMiniguite <ruanminiguite@gmail.com>
2022-07-29 09:45:17 -0300

    :art: Size title

--- scc-frontend-react/src/components/Title.jsx
+++ scc-frontend-react/src/components/Title.jsx
@@ -1,5 +1,5 @@
 export function Title(props) {
     return (
-      <h1 className="pt-10 uppercase scale-125 text-center text-2xl tracking-widest whitespace-normal w-[60%] xl:w-[70%]">{props.title}</h1>
+      <h1 className="pt-10 uppercase scale-125 text-center text-4xl tracking-widest whitespace-normal w-[60%] xl:w-[70%] text">{props.title}</h1>
     );
 }
